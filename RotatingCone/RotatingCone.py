d25972797eb7473280b6eb503ada082179e2e3f8
GEORGE FYLES <2100707@uad.ac.uk>
2025-02-21 22:03:45 +0000

    Finished task 1 and 2

--- RotatingCone/RotatingCone.py
+++ RotatingCone/RotatingCone.py
@@ -1,3 +1,7 @@
+﻿import math
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+#import numpy as np
 
 # Calculate moments of inertia for cone from mass, height and base radius 
 def GetMomentsOfInertia(M, h, r):
@@ -7,7 +11,7 @@ def GetMomentsOfInertia(M, h, r):
     return I1, I2, I3
 
 # Performs Runge-Kutta computation
-def RungeKutta(Wx0, Wy0, Wz0, I1, I2, I3, stepSize, steps):
+def RungeKutta():
     
     # Init omega
     Wxn, Wyn, Wzn = Wx0, Wy0, Wz0
@@ -16,6 +20,10 @@ def RungeKutta(Wx0, Wy0, Wz0, I1, I2, I3, stepSize, steps):
     Y1 = -stepSize * (I3 - I2) / I1
     Y2 = -stepSize * (I1 - I3) / I2
     Y3 = -stepSize * (I2 - I1) / I3
+    
+    # Init arrays for storing W components for plotting
+    WxArray, WyArray, WzArray = [Wxn], [Wyn], [Wzn]
+    WmagArray = [math.sqrt(Wxn*Wxn + Wyn*Wyn + Wzn*Wzn)]
 
     for n in range(0, steps):
 
@@ -35,24 +43,48 @@ def RungeKutta(Wx0, Wy0, Wz0, I1, I2, I3, stepSize, steps):
         Ky4 = Y2 * (Wxn + Kx3) * (Wzn + Kz2)
         Kz4 = Y3 * (Wxn + Kx3) * (Wyn + Ky2)
         
-        Kx = (Kx1 + (2*Kx2) + (2*Kx3) + Kx4)/6
-        Ky = (Ky1 + (2*Ky2) + (2*Ky3) + Ky4)/6
-        Kz = (Kz1 + (2*Kz2) + (2*Kz3) + Kz4)/6
+        Wxn += (Kx1 + (2*Kx2) + (2*Kx3) + Kx4)/6
+        Wyn += (Ky1 + (2*Ky2) + (2*Ky3) + Ky4)/6
+        Wzn += (Kz1 + (2*Kz2) + (2*Kz3) + Kz4)/6
         
-        Wxn += Kx
-        Wyn += Ky
-        Wzn += Kz
+        # Calculate magnitude of W (the angular speed)
+        Wmag = math.sqrt(Wxn*Wxn + Wyn*Wyn + Wzn*Wzn)
+
+        WxArray.append(Wxn)
+        WyArray.append(Wyn)
+        WzArray.append(Wzn)
+        WmagArray.append(Wmag)
+
+        print("Step %s: %s, %s, %s, magnitude: %s" % (n, Wxn, Wyn, Wzn, Wmag))
         
-        print("Step %s: %s, %s, %s" % (n, Wxn, Wyn, Wzn))
+    return WxArray, WyArray, WzArray, WmagArray
 
-    #return Wxn, Wyn, Wzn
+def PlotTask1():
+    # Populate array with values for plot x-axis (time)
+    timeArray = []
+    for i in range(0, steps + 1):
+        timeArray.append(i * stepSize)
 
+    # Plot graph
+    plt.plot(timeArray, WxArray, label="ωx")
+    plt.plot(timeArray, WyArray, label="ωy")
+    plt.plot(timeArray, WzArray, label="ωz")
+    plt.plot(timeArray, WmagArray, label="|ω|")
+    plt.legend(loc="lower right")
+    plt.title("Task 1 plots")
+    plt.xlabel("time (s)")
+    plt.ylabel("ωi (rad/s)")
+    plt.show()    
+
+
+
+# -----========== Main  ==========-----
 
 # Define initial conditions, step size and number of steps
-Wx0, Wy0, Wz0 = 1, 2, 3
+Wx0, Wy0, Wz0 = 3, 1, 2
 mass = 5
 radius = 2
-height = 4
+height = 6
 
 stepSize = 0.1
 steps = 200
@@ -61,7 +93,9 @@ steps = 200
 I1, I2, I3 = GetMomentsOfInertia(mass, height, radius)
 
 # Do the computation
-RungeKutta(Wx0, Wy0, Wz0, I1, I2, I3, stepSize, steps)
+WxArray, WyArray, WzArray, WmagArray = RungeKutta()
 
+# Produce plots of solutions for task 1
+PlotTask1()
 
 #input("\nPress Enter to exit.")
